fa6f74fd5f952582b57ea027a541d18e1e1e5e6e
Alex Delgado <delgadoalex@icloud.com>
2019-08-29 11:51:23 -0500

    HomePage component html structure complete

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,15 @@
 import React from 'react'; 
 
 
-// Home Page Component
+// Functional Home Page Component
+const HomePage = () => (
+    <div className='homepage_wrapper'>
+        <div className='homepage_container'>
+            <img className='homepage_image' src='alex_arbella.jpg' alt='profile headshot'></img>
+            <h2 className='homepage_title'>Alex Delgado</h2>
+        </div>
+    </div>
+); 
 
+export default HomePage; 
  
