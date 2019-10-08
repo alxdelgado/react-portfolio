0b7e47ef988c26e38102f43fc92ad5b918dcea94
Alex Delgado <delgadoalex@icloud.com>
2019-10-08 15:20:47 -0500

    Updates

---
+++ precache-manifest.fefb5f16666e31282b560c107f65d72c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b39c00148d265f155f3ef329e26771",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b24f36d33ee9b48088fd",
+    "url": "/react-portfolio/static/css/main.fc40ddf4.chunk.css"
+  },
+  {
+    "revision": "bd9d7680471d78e3931e",
+    "url": "/react-portfolio/static/js/2.3056de26.chunk.js"
+  },
+  {
+    "revision": "b24f36d33ee9b48088fd",
+    "url": "/react-portfolio/static/js/main.3ed58a93.chunk.js"
+  },
+  {
+    "revision": "9274983667d32818448d",
+    "url": "/react-portfolio/static/js/runtime~main.e2a5686a.js"
+  }
+]);
