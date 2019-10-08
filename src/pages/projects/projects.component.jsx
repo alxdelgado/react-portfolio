d4501de5d2a9147a927206f2c5969b71dfd89077
Alex Delgado <delgadoalex@icloud.com>
2019-10-08 18:04:25 -0500

    removed typing error

--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -6,9 +6,9 @@ const ProjectsPage = props => (
         <div className='projects_container'>
             <h1 className='projects_heading'>Crown Clothing</h1>
             <p>Crown-Clothing is an Ecommerce platform using React, Redux and Firebase. With this application you can log in, choose items and checkout using the Stripe API.</p>
-            <a href='https://crwnclothing-live-shop-popup.herokuapp.com/' target='_blank' rel="noopener noreferrer"git st><img className='crown-clothing-app' src='/Crown-Clothing.png' alt='projectimage'/></a>
+            <a href='https://crwnclothing-live-shop-popup.herokuapp.com/' target='_blank' rel="noopener noreferrer"><img className='crown-clothing-app' src='/Crown-Clothing.png' alt='projectimage'/></a>
         </div>
     </div>
 ); 
 
-export default ProjectsPage; 
+export default ProjectsPage;
