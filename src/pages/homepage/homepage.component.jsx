a995abf412d03cdf41193b7c572fc03945d1bb7e
Alex Delgado <delgadoalex@icloud.com>
2019-10-16 15:52:45 -0500

    changed homepage picture

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,7 +7,7 @@ import './homepage.styles.scss';
 const HomePage = () => (
     <div className='homepage_wrapper'>
         <div className='homepage_container'>
-            <img className='homepage_image' id='alex_sick_photo' src='alex_arbella.jpg' alt='profile headshot'></img>
+            <img className='homepage_image' id='alex_sick_photo' src='alex.jpg' alt='profile headshot'></img>
             <h2 className='homepage_title' id='alex_heading'>Alex Delgado</h2>
         </div>
     </div>
