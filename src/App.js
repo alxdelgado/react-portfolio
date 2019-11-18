6f9a228ee2410354c9a4a6734c45b89278220bbc
Alex Delgado <delgadoalex@icloud.com>
2019-11-18 13:59:41 -0600

    Implementing lazy loading to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import ResumePage from './pages/resume/resume-page.component';
 import NavBar from './components/nav-bar/nav-bar.component';
-import HomePage from './pages/homepage/homepage.component';
+// import HomePage from './pages/homepage/homepage.component';
 import ProjectsPage from './pages/projects/projects.component';   
 import FooterNav from './components/footer/footer.component';
 import SideBar from './components/sidebar/sidebar.component'; 
 
+
+// Lazy Loading
+const HomePage = lazy(() => import ('./pages/homepage/homepage.component'));
 
 // Smart Component
 class App extends React.Component {
   constructor(props) {
     super(props)
 
-    // State 
     this.state = {
       sideBarOpen: false
     } 
@@ -38,7 +40,9 @@ class App extends React.Component {
         <Switch>
           <NavBar sideBarClickHandler={this.sideBarToggleClickHandler} closeSideBarHandler={this.closeSideBarClickHandler} />
           {Content}
-          <Route exact path='/' component={HomePage} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route exact path='/' component={HomePage} />
+          </Suspense>
           <Route path='/resume' component={ResumePage} />
           <Route path='/projects' component={ProjectsPage} />
           <FooterNav />
